67d2854c1226bb9f5e2d7df54370c7768c3c70b7
“zed” <zed@kolk.me>
2021-01-18 22:02:22 -0600

    finsihed

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -7,7 +8,56 @@ class Song
   end
 
   def save
-    self.class.all << self
+    @@all << self
+    self
+  end
+
+  def self.create
+    @@all << new_song = self.new
+    new_song
+  end
+
+  def self.new_by_name title
+    new_song = self.new
+    new_song.name = title
+    new_song
+  end
+
+  def self.create_by_name title
+    self.new_by_name(title).save
+  end
+
+  def self.find_by_name song
+    @@all.each do |x|
+      if x.name == song
+        return x
+      end
+    end
+    nil
+  end
+
+  def self.find_or_create_by_name song
+    yolo = self.find_by_name(song)
+    yolo == nil ? self.create_by_name(song) : yolo
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename filename
+    array = filename.split(" - ")
+    new_song = self.new_by_name(array[1].chomp(".mp3"))
+    new_song.artist_name = array[0]
+    new_song
+  end
+
+  def self.create_from_filename filename
+    self.new_from_filename(filename).save
+  end
+
+  def self.destroy_all
+    @@all = []
   end
 
 end
